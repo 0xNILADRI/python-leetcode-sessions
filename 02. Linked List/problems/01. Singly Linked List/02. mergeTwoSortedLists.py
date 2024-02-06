4846b6a129d7fc5b61a65e38b022726d18afbceb
Niladri Ghosh <niladri1406@gmail.com>
2024-02-06 22:37:32 +0530

    add: problem 21 - added library for optional args

--- 02. Linked List/problems/01. Singly Linked List/02. mergeTwoSortedLists.py
+++ 02. Linked List/problems/01. Singly Linked List/02. mergeTwoSortedLists.py
@@ -1,12 +1,16 @@
 # 21. Merge Two Sorted Lists
 # link : https://leetcode.com/problems/merge-two-sorted-lists/description/
 
+from typing import Optional
+
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
     def mergeTwoLists(
         self, list1: Optional[ListNode], list2: Optional[ListNode]
