d8910ccbd14e6d83b922aeb74cde86c2c5e6ef14
Niladri Ghosh <niladri1406@gmail.com>
2024-02-06 22:36:59 +0530

    add: problem 206 - added library for optional args

--- 02. Linked List/problems/01. Singly Linked List/01. reverseLinkedList.py
+++ 02. Linked List/problems/01. Singly Linked List/01. reverseLinkedList.py
@@ -1,12 +1,16 @@
 # 206. Reverse Linked List
 # link : https://leetcode.com/problems/reverse-linked-list/description/
 
+from typing import Optional
+
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev, curr = None, head
