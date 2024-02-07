1220358081fe10ae5295f6b0fc9ea9fc8a9195e2
Niladri Ghosh <niladri1406@gmail.com>
2024-02-07 16:17:33 +0530

    add: problem 707

---
+++ 02. Linked List/problems/02. Doubly Linked List/01. designLinkedList.py
@@ -0,0 +1,70 @@
+# 707. Design Linked List
+# link : https://leetcode.com/problems/design-linked-list/description/
+
+
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.prev = None
+
+
+class MyLinkedList:
+    def __init__(self):
+        self.left = ListNode(0)
+        self.right = ListNode(0)
+        self.left.next = self.right
+        self.right.prev = self.left
+
+    def get(self, index: int) -> int:
+        curr = self.left.next
+        while index > 0 and curr:
+            curr = curr.next
+            index -= 1
+        if curr and curr != self.right and index == 0:
+            return curr.val
+        return -1
+
+    def addAtHead(self, val: int) -> None:
+        node, next, prev = ListNode(val), self.left.next, self.left
+        prev.next = node
+        next.prev = node
+        node.next = next
+        node.prev = xt
+        if curr and curr != self.right and index == 0:
+            next, prev = curr.next, curr.prev
+            prev.next = next
+            next.prev = prev
+
+    # Your MyLinkedList object will be instantiated and called as such:
+    # obj = MyLinkedList()
+    # param_1 = obj.get(index)
+    # obj.addAtHead(val)
+    # obj.addAtTail(val)
+    # obj.addAtIndex(index,val)
+    # obj.deleteAtIndex(index) prev
+
+    def addAtTail(self, val: int) -> None:
+        node, next, prev = ListNode(val), self.right, self.right.prev
+        prev.next = node
+        next.prev = node
+        node.next = next
+        node.prev = prev
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        curr = self.left.next
+        while index > 0 and curr:
+            index -= 1
+            curr = curr.next
+        if curr and index == 0:
+            node, next, prev = ListNode(val), curr, curr.prev
+            prev.next = node
+            next.prev = node
+            node.next = next
+            node.prev = prev
+
+    def deleteAtIndex(self, index: int) -> None:
+        curr = self.left.next
+        while index > 0 and curr:
+            index -= 1
+            curr = curr.ne
